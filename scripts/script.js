18aad570f41cdf788ecc2a3c96387f6c4a04122c
Tanzil <tanzil.animashaun@gmail.com>
2020-01-02 01:00:54 +0000

    add javascript animation to header

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
-
-        window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction()};
 
         var header = document.getElementById("mainNav");
         var accounts = document.getElementById("accountNav"); 
@@ -59,56 +58,3 @@ const questionAnswer = () => {
 }
 
 questionAnswer();
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-const keyToggle = () => {
-    document.addEventListener('keydown', (e) => {
-    if (e.code === "ArrowLeft")  {plusSlides(-1)}
-  else if (e.code === "ArrowRight") {plusSlides(1)}
-})}
-
-keyToggle();
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-
-var timer = setInterval(plusSlides,7000, 1);
-var pauseButton = document.querySelector(".pause");
-var playing = true;
-
-function pausePlaySlideshow(){
-    if (playing) {
-	pauseButton.innerHTML = '&#9658';
-	playing = false;
-    clearInterval(timer);
-    }
-    else {
-        pauseButton.innerHTML = '&#10074;&#10074;';
-        playing = true;
-        timer =
-        setInterval(plusSlides,7000, 1);
-    }
-}
